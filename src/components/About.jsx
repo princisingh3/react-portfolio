5981174f6d19bfe14ec531106a660ed4a198d5e7
Princii siingh <ps8531352@gmail.com>
2026-08-25 15:39:16 +0530

    Add About component with personal information

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,1 +1,26 @@
+function About() {
+  return (
+    <section id="about" className="about">
+      <h2>About Me</h2>
 
+      <p>
+        Hello! I'm Princi Singh, a B.Tech Computer Science and Design
+        student. I am interested in web development and learning new
+        technologies.
+      </p>
+
+      <p>
+        I enjoy creating useful and creative projects using HTML, CSS,
+        JavaScript and React.
+      </p>
+
+      <div className="about-info">
+        <p><strong>Education:</strong> B.Tech in CSD</p>
+        <p><strong>College:</strong> RR Institutions of Modern Technology</p>
+        <p><strong>Location:</strong> Lucknow, Uttar Pradesh</p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
